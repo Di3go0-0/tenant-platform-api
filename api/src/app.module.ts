d60cda39bcd47c1f33ade21562ab26d12249d759
Di3go0-0 <d.rincon@utp.edu.co>
2026-04-19 21:08:53 -0500

    Add health check and folder structure

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+import { ConfigModule } from './infrastructure/config/index.js';
+import { DatabaseModule } from './infrastructure/database/index.js';
+import { PlsqlModule } from './core/plsql/index.js';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule, DatabaseModule, PlsqlModule],
   controllers: [AppController],
   providers: [AppService],
 })
