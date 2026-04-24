23ef556fc9aeee2ec203be9bbec9e6d127d50bf8
Di3go0-0 <d.rincon@utp.edu.co>
2026-04-23 21:06:38 -0500

    Add permissions module

---
+++ api/src/modules/permissions/permissions.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { PermissionsController } from './permissions.controller.js';
+import { PermissionsService } from './permissions.service.js';
+import { PermissionsRepository } from './repositories/permissions.repository.js';
+import { PermissionsGuard } from '../../common/guards/permissions.guard.js';
+import { AuthModule } from '../auth/index.js';
+import { TenantsModule } from '../tenants/index.js';
+
+@Module({
+  imports: [AuthModule, TenantsModule],
+  controllers: [PermissionsController],
+  providers: [PermissionsService, PermissionsRepository, PermissionsGuard],
+  exports: [PermissionsService, PermissionsGuard],
+})
+export class PermissionsModule {}
